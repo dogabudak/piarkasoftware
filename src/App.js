3f94027b9069580b4e8dc002bdc3c001e2fca789
doga <doga.budak@check24.de>
2020-12-25 21:19:52 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -6,16 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        Piarka Software is coming to our earth...
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://www.dogabudak.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        Doga Budak
         </a>
       </header>
     </div>
